5e05d90f0f37df49c42f230e9844b3f408acb288
llh5432 <ihlee@imex.co.kr>
2019-10-07 18:37:34 +0900

    Product 완료단계 기획안 수정 후 order table 추가예정

---
+++ src/main/kotlin/com/example/demo/controller/OrderController.kt
@@ -0,0 +1,13 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OrderController {
+
+    // 주문 목록을 받아서 디비에 저장하는 기능
+
+    // 내 주문 목록 보는 기능
+
+    // 주문 상세
+}
