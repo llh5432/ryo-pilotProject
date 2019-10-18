aa3d8515e66ab9e9b6dc8e8516a42b9e65a81d3e
llh5432 <ihlee@imex.co.kr>
2019-10-18 17:48:23 +0900

    pliot project vue 시작

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="btn btn-primary">되나</button>
   </div>
 </template>
 
@@ -15,7 +16,6 @@ export default {
   }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-container id="containerSet" class="bv-d-md-down-none">
+        <b-row id="rowSet" class="justify-content-md-center">
+            <div id="login">
+                <b-card id="cardSet" >
+                    <form>
+                        <label>Your 계정을 입력하라</label>
+                        <b-form-input type="text" placeholder="Account"/><br>
+                        <b-form-input type="password" placeholder="Password"/><br>
+                            <b-button id="btnSet" variant="outline-primary">로그인</b-button><br>
+                            <label><a href="HelloWorld.vue">아이디 찾기</a></label><br>
+                            <label><a href="HelloWorld.vue">패스워드 찾기</a></label>
+                    </form>
+                </b-card>
+            </div>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "LoginForm",
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 400px;
+        margin-top: 25%;
+        border: black solid;
+    }
+    #btnSet {
+        text-justify: auto;
+    }
+</style>
