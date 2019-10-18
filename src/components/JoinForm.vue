b79b230d633e4118104a9e8461a1c212c004fee9
llh5432 <ihlee@imex.co.kr>
2019-10-18 17:46:06 +0900

    pliot project vue 시작

---
+++ src/components/JoinForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <b-container id="containerSet" class="bv-d-md-down-none">
+        <b-row id="rowSet" class="justify-content-md-center">
+            <div id="login">
+                <b-card id="cardSet" >
+                    <form id="formSet">
+                        <label>Your 계정을 입력하라</label>
+                        <div id="formBodySet">
+                            <b-form-input type="text" placeholder="Account"/><br>
+                            <b-form-input type="password" placeholder="Password"/><br>
+                        </div>
+                        <div id="formFootSet">
+                            <b-button variant="outline-primary">로그인</b-button><br>
+                            <label><a href="HelloWorld.vue">아이디 찾기</a></label><br>
+                            <label><a href="HelloWorld.vue">패스워드 찾기</a></label>
+                        </div>
+                    </form>
+                </b-card>
+            </div>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "JoinForm",
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 400px;
+        margin-top: 25%;
+        border: black solid;
+    }
+    #formSet {
+        text-align: center;
+    }
+</style>
